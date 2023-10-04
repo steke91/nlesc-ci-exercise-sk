1e5fc5bf2b1d93f942038b842a0188bff145c9a8
Steven Ketelaars <stevenketelaars@gmail.com>
2023-10-04 15:34:38 +0200

    Add error to example.py

--- example.py
+++ example.py
@@ -5,5 +5,5 @@ def test_add():
     assert add(2, 3) == 5
 
 def subtract(a, b):
-    return a + b  # do not change this line until prompted to do so.
+    return a + b   do not change this line until prompted to do so.
 
