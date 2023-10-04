f5aba142c2592178cf213473e6aa1356f53be99f
RenskeW <renskedewit1998@gmail.com>
2023-10-04 15:49:56 +0200

    small change to enable ci

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ def test_add():
     assert add(2, 3) == 5
 
 def subtract(a, b):
-    return a - b   # fixed :)
+    return a - b   # fixed
 
 @pytest.mark.parametrize("test_input1, test_input2, expected", [(5, 0, 5), (4.3, 0.9, 3.4)]) 
 def test_subtract(test_input1, test_input2, expected):
